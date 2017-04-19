7e684d017b9998b0551c5322b883dfa37ac65d9e
Teresa Nesteby <teresa.nesteby@gmail.com>
2017-04-19 14:10:17 -0700

    connect button to track and identify

--- Analytics-Teresa/AppDelegate.m
+++ Analytics-Teresa/AppDelegate.m
@@ -16,62 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    
-    NSURLSession *session = [NSURLSession sharedSession];
-    
-
-//-----------------------------------------
-//identify call
-    NSDictionary* identify = @{ @"userId": @"78",
-                                @"traits": @{ @"email": @"teresa@segment.com"}};
-    NSError* error;
-    NSData* requestData = [NSJSONSerialization dataWithJSONObject:identify options:0 error:&error];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://api-segment-io-cm9xu7l447kc.runscope.net/v1/identify"]];
-
-//-----------------------------------------
-//track call
-//    NSDictionary* track = @{ @"userId": @"3333",
-//                             @"event": @"objc track tests 22222",
-//                             @"properties": @{ @"property key": @"property value"}};
-//    
-//    
-//    NSError* error;
-//    NSData* requestData = [NSJSONSerialization dataWithJSONObject:track options:0 error:&error];
-//    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://api-segment-io-cm9xu7l447kc.runscope.net/v1/track"]];
-//    
-//  --------------------------------------
-
-    
-    NSString* writeKey=@"4txshy8l73";
-    NSString *rawHeader = [writeKey stringByAppendingString:@":"];
-    NSData *userPasswordData = [rawHeader dataUsingEncoding: NSUTF8StringEncoding];
-    NSString *encodedWriteKey = [userPasswordData base64EncodedStringWithOptions:0];
-    NSString *authorizationHeader = [@"Basic " stringByAppendingString:encodedWriteKey];
-    NSLog(@"%@", authorizationHeader);
-    
-    
-    
-    
-//sent with every call
-     
-     
-     
-    [request setHTTPMethod:@"POST"];
-    [request setHTTPBody:requestData];
-    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    [request setValue:authorizationHeader forHTTPHeaderField:@"Authorization"];
-    
-    
-    
-//The error object passed to the completion handler is only populated, not nil, if the request failed or encountered an error
-    NSURLSessionTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-    }];
-
-//By default it is paused - must resume
-    [task resume];
-    return YES;
-
+       return YES;
     
 }
 
--- Analytics-Teresa/ViewController.m
+++ Analytics-Teresa/ViewController.m
@@ -26,15 +26,72 @@
 }
 
 - (IBAction)identifyCall {
-    NSString* hello = @"hello";
-    NSLog(@"%@", hello);
+    NSURLSession *session = [NSURLSession sharedSession];
+
+    NSDictionary* identify = @{ @"userId": @"99",
+                                @"traits": @{ @"email": @"teresa@segment.com"}};
+    NSError* error;
+    NSData* requestData = [NSJSONSerialization dataWithJSONObject:identify options:0 error:&error];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://api-segment-io-cm9xu7l447kc.runscope.net/v1/identify"]];
+    
+    NSString* writeKey=@"4txshy8l73";
+    NSString *rawHeader = [writeKey stringByAppendingString:@":"];
+    NSData *userPasswordData = [rawHeader dataUsingEncoding: NSUTF8StringEncoding];
+    NSString *encodedWriteKey = [userPasswordData base64EncodedStringWithOptions:0];
+    NSString *authorizationHeader = [@"Basic " stringByAppendingString:encodedWriteKey];
+    NSLog(@"%@", authorizationHeader);
+    
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:requestData];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request setValue:authorizationHeader forHTTPHeaderField:@"Authorization"];
+    
+    
+    
+    //The error object passed to the completion handler is only populated, not nil, if the request failed or encountered an error
+    NSURLSessionTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+    }];
+    
+    //By default it is paused - must resume
+    [task resume];
+
     
 }
 
 
 - (IBAction)trackCall {
-    NSString* goodbye = @"goodbye";
-    NSLog(@"%@", goodbye);
+    NSURLSession *session = [NSURLSession sharedSession];
+    
+    NSDictionary* track = @{ @"userId": @"3333",
+                             @"event": @"objc track tests 22222",
+                             @"properties": @{ @"property key": @"property value"}};
+    
+    
+    NSError* error;
+    NSData* requestData = [NSJSONSerialization dataWithJSONObject:track options:0 error:&error];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://api-segment-io-cm9xu7l447kc.runscope.net/v1/track"]];
+    
+    NSString* writeKey=@"4txshy8l73";
+    NSString *rawHeader = [writeKey stringByAppendingString:@":"];
+    NSData *userPasswordData = [rawHeader dataUsingEncoding: NSUTF8StringEncoding];
+    NSString *encodedWriteKey = [userPasswordData base64EncodedStringWithOptions:0];
+    NSString *authorizationHeader = [@"Basic " stringByAppendingString:encodedWriteKey];
+    NSLog(@"%@", authorizationHeader);
+    
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:requestData];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request setValue:authorizationHeader forHTTPHeaderField:@"Authorization"];
+    
+    
+    
+    //The error object passed to the completion handler is only populated, not nil, if the request failed or encountered an error
+    NSURLSessionTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+    }];
+    
+    //By default it is paused - must resume
+    [task resume];
+
 }
 
 
