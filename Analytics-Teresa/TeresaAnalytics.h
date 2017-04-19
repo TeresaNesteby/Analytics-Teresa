eff41c551c3f578333bfbdab3d7e6461cd5c096c
Teresa Nesteby <teresa.nesteby@gmail.com>
2017-04-19 14:32:46 -0700

    adds TeresaAnalytics class

---
+++ Analytics-Teresa/TeresaAnalytics.h
@@ -0,0 +1,17 @@
+//
+//  TeresaAnalytics.h
+//  Analytics-Teresa
+//
+//  Created by Teresa Nesteby on 4/19/17.
+//  Copyright © 2017 Teresa Nesteby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TeresaAnalytics : NSObject
+
+- (void)identify;
+- (void)track;
+
+
+@end
--- Analytics-Teresa/ViewController.m
+++ Analytics-Teresa/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "TeresaAnalytics.h"
 
 @interface ViewController ()
 
@@ -26,75 +27,17 @@
 }
 
 - (IBAction)identifyCall {
-    NSURLSession *session = [NSURLSession sharedSession];
-
-    NSDictionary* identify = @{ @"userId": @"99",
-                                @"traits": @{ @"email": @"teresa@segment.com"}};
-    NSError* error;
-    NSData* requestData = [NSJSONSerialization dataWithJSONObject:identify options:0 error:&error];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://api-segment-io-cm9xu7l447kc.runscope.net/v1/identify"]];
-    
-    NSString* writeKey=@"4txshy8l73";
-    NSString *rawHeader = [writeKey stringByAppendingString:@":"];
-    NSData *userPasswordData = [rawHeader dataUsingEncoding: NSUTF8StringEncoding];
-    NSString *encodedWriteKey = [userPasswordData base64EncodedStringWithOptions:0];
-    NSString *authorizationHeader = [@"Basic " stringByAppendingString:encodedWriteKey];
-    NSLog(@"%@", authorizationHeader);
-    
-    [request setHTTPMethod:@"POST"];
-    [request setHTTPBody:requestData];
-    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    [request setValue:authorizationHeader forHTTPHeaderField:@"Authorization"];
-    
-    
-    
-    //The error object passed to the completion handler is only populated, not nil, if the request failed or encountered an error
-    NSURLSessionTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-    }];
-    
-    //By default it is paused - must resume
-    [task resume];
-
-    
+    TeresaAnalytics *analytics = [[TeresaAnalytics alloc] init];
+    [analytics identify];
 }
 
 
 - (IBAction)trackCall {
-    NSURLSession *session = [NSURLSession sharedSession];
+    TeresaAnalytics *analytics = [[TeresaAnalytics alloc] init];
+    [analytics track];
     
-    NSDictionary* track = @{ @"userId": @"3333",
-                             @"event": @"objc track tests 22222",
-                             @"properties": @{ @"property key": @"property value"}};
-    
-    
-    NSError* error;
-    NSData* requestData = [NSJSONSerialization dataWithJSONObject:track options:0 error:&error];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://api-segment-io-cm9xu7l447kc.runscope.net/v1/track"]];
-    
-    NSString* writeKey=@"4txshy8l73";
-    NSString *rawHeader = [writeKey stringByAppendingString:@":"];
-    NSData *userPasswordData = [rawHeader dataUsingEncoding: NSUTF8StringEncoding];
-    NSString *encodedWriteKey = [userPasswordData base64EncodedStringWithOptions:0];
-    NSString *authorizationHeader = [@"Basic " stringByAppendingString:encodedWriteKey];
-    NSLog(@"%@", authorizationHeader);
-    
-    [request setHTTPMethod:@"POST"];
-    [request setHTTPBody:requestData];
-    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    [request setValue:authorizationHeader forHTTPHeaderField:@"Authorization"];
-    
-    
-    
-    //The error object passed to the completion handler is only populated, not nil, if the request failed or encountered an error
-    NSURLSessionTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-    }];
-    
-    //By default it is paused - must resume
-    [task resume];
 
 }
-
-
 
 
 @end
